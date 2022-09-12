1a234833bad95bcd0d5c582a5c84fb5fcf7c126f
anxelius <mximadzko@gmail.com>
2022-09-12 13:57:54 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-*/**
-  * main - Entry point
-  * Program uses puts instead of printf for text output
-  */
+/**
+ * main - Entry point
+ * Program uses puts instead of printf for text output
+ */
 int main(void)
 {
   char Mystr[] = "\"Programming is like building a multilingual puzzle";
