fb04431bd6974da6a0b58223ac08541c22857109
anxelius <mximadzko@gmail.com>
2022-09-12 14:15:19 -0700

    C programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**main -Entry
+*Program prints using printf
+*Return: ALways 0 (Success)
+**/
+int main(void)
+{printf("with proper grammar, but ther outcome is a piece of art,\n");
+return (0);
+}
